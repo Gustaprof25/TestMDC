e950aed375496fd3f9527a7842b1f783dbe772fc
Gustavo de Lima Pereira <66458779+Gustaprof25@users.noreply.github.com>
2022-04-25 22:31:44 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/mdc/MathUtil.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mdc;
+
+/**
+ *
+ * @author aluno
+ */
+public class MathUtil {
+    public static double mdc(double a, double b){
+        if(b > 0){
+            if(a % b == 0)
+                return b;
+        }
+        
+        return -1;
+    }
+}
